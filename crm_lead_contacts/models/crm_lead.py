9ad692f6bfffca7807bb580a088c0fd154408a2c
onaumenko-pixel <o.naumenko@rayton.com.ua>
2026-02-24 13:36:58 +0100

    Update crm_lead.py

--- crm_lead_contacts/models/crm_lead.py
+++ crm_lead_contacts/models/crm_lead.py
@@ -1,11 +1,20 @@
 from odoo import models, fields
 
+
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     contact_ids = fields.One2many(
         'res.partner',
-        'parent_id',
-        string='Contact Persons',
-        domain=[('type', '=', 'contact')]
+        'lead_id',
+        string='Contact Persons'
+    )
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    lead_id = fields.Many2one(
+        'crm.lead',
+        string='Lead'
     )
