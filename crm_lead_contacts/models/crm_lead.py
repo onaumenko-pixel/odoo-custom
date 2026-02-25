e40ac0347bc57a9ddcfae6607b5184f1bfd79363
onaumenko-pixel <o.naumenko@rayton.com.ua>
2026-02-25 09:18:48 +0100

    Update crm_lead.py

--- crm_lead_contacts/models/crm_lead.py
+++ crm_lead_contacts/models/crm_lead.py
@@ -1,31 +1,21 @@
-from odoo import models, fields, _
+def action_add_company_contact(self):
+    self.ensure_one()
+    if not self.partner_id:
+        return False
 
-class CrmLead(models.Model):
-    _inherit = "crm.lead"
+    partner_form = self.env.ref("base.view_partner_form")
 
-    # показываем контакты компании (дочерние контакты партнера)
-    contact_ids = fields.One2many(
-        comodel_name="res.partner",
-        inverse_name="parent_id",
-        string="Contacts",
-        related="partner_id.child_ids",
-        readonly=True,
-    )
-
-    def action_add_company_contact(self):
-        self.ensure_one()
-        if not self.partner_id:
-            return False
-
-        return {
-            "type": "ir.actions.act_window",
-            "name": _("Create Contact"),
-            "res_model": "res.partner",
-            "view_mode": "form",
-            "target": "new",
-            "context": {
-                "default_parent_id": self.partner_id.id,   # привязка к компании
-                "default_type": "contact",                 # контактное лицо
-                "default_company_type": "person",          # физ. лицо
-            },
-        }
+    return {
+        "type": "ir.actions.act_window",
+        "name": _("Create Contact"),
+        "res_model": "res.partner",
+        "view_mode": "form",
+        "views": [(partner_form.id, "form")],
+        "target": "new",
+        "context": {
+            "default_parent_id": self.partner_id.id,
+            "default_company_type": "person",
+            "default_is_company": False,
+            "default_type": "contact",
+        },
+    }
