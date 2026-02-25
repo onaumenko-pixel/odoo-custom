633efc86f058315adae2ddcee63a1583baa06d6d
onaumenko-pixel <o.naumenko@rayton.com.ua>
2026-02-25 10:38:05 +0100

    Update crm_lead.py

--- crm_lead_contacts/models/crm_lead.py
+++ crm_lead_contacts/models/crm_lead.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class CrmLead(models.Model):
@@ -6,19 +6,10 @@ class CrmLead(models.Model):
 
     contact_ids = fields.One2many(
         comodel_name="res.partner",
-        compute="_compute_contact_ids",
+        inverse_name="lead_id",
         string="Contacts",
+        domain=[
+            ("company_type", "=", "person"),
+            ("type", "=", "contact"),
+        ],
     )
-
-    @api.depends("partner_id")
-    def _compute_contact_ids(self):
-        for lead in self:
-            if not lead.partner_id:
-                lead.contact_ids = False
-            else:
-                contacts = self.env["res.partner"].search([
-                    ("parent_id", "=", lead.partner_id.id),
-                    ("company_type", "=", "person"),
-                    ("type", "=", "contact"),
-                ])
-                lead.contact_ids = contacts
